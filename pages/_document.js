18f9a1f58df22bf6c5d89a1c890ff03614aea120
mxmgamaleldin <mxmgamaleldin@gmail.com>
2021-12-19 12:17:01 +0200

    Custom font on production

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="/fonts/fonts.css"
+            rel="stylesheet"
+            as="font"
+            crossOrigin=""
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
