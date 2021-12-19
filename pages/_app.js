e553cc0d618e79bf98f512f89c21a8ff44833142
mxmgamaleldin <mxmgamaleldin@gmail.com>
2021-12-19 12:21:09 +0200

    Custom font on production

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+import { mdComponents } from '../components/mdx'
+import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <MDXProvider components={mdComponents}>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        @font-face {
+          font-family: "SubSpace";
+          src: url("/fonts/SubSpace-Regular.ttf");
+          font-style: normal;
+          font-weight: 400;
+          font-display: swap;
+        }
+      `}</style>
+    </MDXProvider>
+  )
 }
+
+export default MyApp
