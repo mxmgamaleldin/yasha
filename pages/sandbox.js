a57a1c4b697c36710917fc3b8711d77c0b0a21c2
mxmgamaleldin <mxmgamaleldin@gmail.com>
2021-12-25 10:22:39 +0200

    Refactoring.

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -9,40 +9,28 @@ export default function Sandbox() {
 	
 	useEffect(() => {
 		
-		const heroTL = gsap.timeline();
-		heroTL.to("#curtain", {
-			yPercent: -100,
-			ease: "circ.inOut",
-			duration: 2
-		})
-		.fromTo("#hero", {
-			scale: 1.5
-		}, {
-			scale: 1,
-			duration: 5,
-			ease: "none"
-		}, ">-0.75");
-		
-		heroTL.to("#hero-img-wrapper", {
-			width: "50vw",
-			height: "50vh",
-			duration: 30,
-		}, ">-3");
+//		const heroTL = gsap.timeline();
+//		heroTL.("#hero-wrapper", {
+//			duration: 1,
+//			height: "auto"
+//		});
 
 	}, []);
 	
   return (<>
-			<div className="w-full min-h-screen">
-				<div id="curtain" className="bg-black w-full min-h-screen absolute z-10"/>
-				<Image
-					id="hero"
-					priority
-					alt=""
-					src="/images/hero.webp"
-					layout="fill"
-					objectFit="cover"
-					quality={100}
-				/>			
+			<div className="w-full min-h-screen bg-black">
+				<div id="hero-wrapper">
+					<Image
+						priority
+						id="hero"
+						alt=""
+						src="/images/hero-2.jpeg"
+						layout="fill"
+						objectFit="cover"
+						objectPosition="0 0"
+						quality={100}
+					/>						
+				</div>		
 			</div>
   </>);
 }
